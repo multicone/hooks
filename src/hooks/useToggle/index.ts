7073aa10404d296e3d7c353c5ba990c9dc4a3867
nawab <nawabkhairuzzaman@gmail.com>
2021-12-15 01:12:39 +0600

    ready for production

--- src/hooks/useToggle/index.ts
+++ src/hooks/useToggle/index.ts
@@ -2,7 +2,27 @@ import { useState } from 'react';
 
 type IUseToogle = (boolean | ((value?: boolean) => void))[];
 
-export const useToggle = (defaultValue: boolean): IUseToogle => {
+/**
+ * @description Toggle hook for react.
+ * @param {boolean} defaultValue
+ * @return {*}  {IUseToogle}
+ * @author nawab69
+ * @tutorial https://github.com/multicone/hooks
+ * @example
+ *    function ToggleComponent() {
+ *    const [value, toggleValue] = useToggle(false);
+ *      return (
+ *         <div>
+ *          <div>{value.toString()}</div>
+ *           <button onClick={toggleValue}>Toggle</button>
+ *           <button onClick={() => toggleValue(true)}>Make True</button>
+ *           <button onClick={() => toggleValue(false)}>Make False</button>
+ *         </div>
+ *       );
+ *     }
+ */
+
+const useToggle = (defaultValue: boolean): IUseToogle => {
   const [value, setValue] = useState<boolean>(defaultValue);
 
   const toogleValue = (value?: boolean) => {
@@ -11,3 +31,5 @@ export const useToggle = (defaultValue: boolean): IUseToogle => {
 
   return [value, toogleValue];
 };
+
+export { useToggle };
